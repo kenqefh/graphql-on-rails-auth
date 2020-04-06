eb5cb91dcf9f118fff1d2aad91eadb6464fd254f
Collins Ruto <borecollins@gmail.com>
2020-04-06 10:10:17 +0300

    Create UserType

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, Id, null: false
+    field :email, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :full_name, String, null: false
+    field :created_at, GraphQL::Types::ISO5601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO5601DateTime, null: false
+
+    def full_name
+      [object.first_name, object.last_name].compact.join(" ")
+    end
+  end
+end
